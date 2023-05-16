d6ec6c7d9852ce4bcac7a783138b66af3c04fa7a
Renan <rs.miranda93@gmail.com>
2023-05-15 22:13:10 -0400

    README update and adding comments

--- README.md
+++ README.md
@@ -1,1 +1,71 @@
-# -SVG-Logo-Maker
+# README-GENERATOR
+[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
+
+```
+GIVEN a command-line application that accepts user input
+WHEN I am prompted for text
+THEN I can enter up to three characters
+WHEN I am prompted for the text color
+THEN I can enter a color keyword (OR a hexadecimal number)
+WHEN I am prompted for a shape
+THEN I am presented with a list of shapes to choose from: circle, triangle, and square
+WHEN I am prompted for the shape's color
+THEN I can enter a color keyword (OR a hexadecimal number)
+WHEN I have entered input for all the prompts
+THEN an SVG file is created named `logo.svg`
+AND the output text "Generated logo.svg" is printed in the command line
+WHEN I open the `logo.svg` file in a browser
+THEN I am shown a 300x200 pixel image that matches the criteria I entered
+``` 
+
+## Table of Contents
+  * [Installation](#installation)
+  * [Usage](#usage)
+  * [License](#license)
+  * [Contributing](#contributing)
+  * [Testing](#testing)
+  * [Questions](#questions)
+
+
+
+## Installation
+
+To install this app you will need to clone the repository from github, install node.js and then run npm i inquirer@8.2.4 in the terminal. Then type node index.js so the questions will be promped.
+
+
+
+## Usage
+
+To run the program, run the code below in Node according to the terminal prompts:
+
+<!-- <img src="./assets/README.GIF" widht=400 height=400> -->
+https://drive.google.com/file/d/1mswQzBpkhkpySwTuFXelhGgxlttTK5Yp/view?usp=share_link
+
+
+
+
+## License
+
+This application is covered by the MIT license
+
+
+
+
+## Contributing
+
+Any improvements or advice are always welcome.
+
+
+
+
+## Testing
+
+Run npm test to check if everything is rugnning properly without errors.
+
+
+
+## Questions
+
+[Email me](mailto:rs.miranda93@gmail.com)
+
+[Review my other projects on GitHub.](https://www.github.com/renansm93)
--- index.js
+++ index.js
@@ -1,11 +1,6 @@
-// Inquirer (node package manager) import
+// Include packages needed for this application
 const inquirer = require("inquirer");
-
-// File system module (node package manager) import
 const fs = require("fs");
-
-// Importing classes from ./lib/shapes directory
-const { Triangle, Square, Circle } = require("./lib/shapes");
 
 //npm package to vaild for CSS3 compatible color value.
 const isCss3Color = require("is-css3-color");
@@ -13,97 +8,92 @@ const isCss3Color = require("is-css3-color");
 // Function call to initialize app
 init();
 
-// Create a function to initialize app
+// Function that creates a matrix with the information that the user inputs
 function init() {
   inquirer
     .prompt([
-      // Text prompt
+      
       {
         type: "input",
-        message:
-          "What text would you like you logo to display? (Enter up to three characters)",
         name: "text",
+        message: "Please enter 1-3 characters for your logo: ",
       },
-      // Text color prompt
+      
       {
         type: "input",
-        message:
-          "Choose text color (Enter color keyword OR a hexadecimal number)",
         name: "textColor",
+        message: "Please enter text color keyword or a hexadecimal number as the logo's text color: ",
       },
-      // Shape choice prompt
+      
       {
         type: "list",
-        message: "What shape would you like the logo to render?",
-        choices: ["Triangle", "Square", "Circle"],
         name: "shape",
+        message: "Please choose logo shape: ",
+        choices: ["Triangle", "Square", "Circle"],        
       },
-      // Shape color prompt
+      
       {
         type: "input",
-        message:
-          "Choose shapes color (Enter color keyword OR a hexadecimal number)",
-        name: "shapeBackgroundColor",
+        name: "logoColor",
+        message: "Please enter a color keyword or a hexadecimal number as the logo's background color: ",        
       },
     ])
     .then((answers) => { 
       let wtextColor = answers.textColor.toLowerCase();
-      let wshapeBackgroundColor = answers.shapeBackgroundColor.toLowerCase();
-      // Error handling for text prompt (user must enter 3 characters or less for logo to generate)
+      let wlogoColor = answers.logoColor.toLowerCase();
+
+      // handling of errors that occurred during the entry of information
       if (answers.text == "") {
-        console.log("Text is Empty");
+        console.log("");
+        console.log("Input text cannot be empty");
+        console.log("");
         init();
       } else if  (answers.text.length > 3) {
-          console.log("Must enter a value of no more than 3 characters");
+          console.log("");
+          console.log("Logo text cannot be more than 3 characters");
+          console.log("");
           init();
       } else if  (!isCss3Color(wtextColor)) {
+          console.log("");
           console.log("Please enter a valid css color keyword or hex code for text color");
+          console.log("");
           init();      
-      } else if  (!isCss3Color(wshapeBackgroundColor)) {
-          console.log("Please enter a valid css color keyword or hex code for shape background");
+      } else if  (!isCss3Color(wlogoColor)) {
+          console.log("");
+          console.log("Please enter a valid css color keyword or hex code for the logo's background color");
+          console.log("");
           init();
       } else {
-        // Calling write file function to generate SVG file
-        writeToFile("logo.svg", answers);
+          writeToFile("logo.svg", answers);
       }
     });
 }
 
-// Function writes the SVG file using user answers from inquirer prompts
+// Function to write the SVG file with the user answers
 function writeToFile(fileName, answers) {
-  // File starts as an empty string
-  let svgString = "";
-  // Sets width and height of logo container
-  svgString =
-    '<svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg">';
-  // <g> tag wraps <text> tag so that user font input layers on top of polygon -> not behind
-  svgString += "<g>";
-  // Takes user input for shape choice and inserts it into SVG file
-  svgString += `${answers.shape}`;
+  
+  let generateSVG = "";
+ 
+  generateSVG = '<svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg"><g>';
+  generateSVG += `${answers.shape}`;
 
-  // Conditional check takes users input from choices array and then adds polygon properties and shape color to SVG string
-  let shapeChoice;
   if (answers.shape === "Triangle") {
-    shapeChoice = new Triangle();
-    svgString += `<polygon points="150, 18 244, 182 56, 182" fill="${answers.shapeBackgroundColor}"/>`;
+      generateSVG += `<polygon points="150,5  274,172  26,172" fill="${answers.logoColor}"/>`;
   } else if (answers.shape === "Square") {
-    shapeChoice = new Square();
-    svgString += `<rect x="73" y="40" width="160" height="160" fill="${answers.shapeBackgroundColor}"/>`;
+      generateSVG += `<rect x="60" y="13" width="180" height="180" fill="${answers.logoColor}"/>`;
   } else {
-    shapeChoice = new Circle();
-    svgString += `<circle cx="150" cy="115" r="80" fill="${answers.shapeBackgroundColor}"/>`;
+      generateSVG += `<circle cx="150" cy="100" r="90" fill="${answers.logoColor}"/>`;
   }
 
-  // <text> tag gives rise to text alignment, text-content/text-color taken in from user prompt and gives default font size of "40"
-  svgString += `<text x="150" y="130" text-anchor="middle" font-size="40" fill="${answers.textColor}">${answers.text}</text>`;
-  // Closing </g> tag
-  svgString += "</g>";
-  // Closing </svg> tag
-  svgString += "</svg>";
+  generateSVG += `<text x="150" y="114" text-anchor="middle" font-size="45" fill="${answers.textColor}">${answers.text}</text>`;
+  generateSVG += "</g>";
+  generateSVG += "</svg>";
 
   // Using file system module to generate svg file, takes in file name given in the init function, the svg string, and a ternary operator which handles logging any errors, or a "Generated logo.svg" message to the console  
-  fs.writeFile(fileName, svgString, (err) => {
+  fs.writeFile(fileName, generateSVG, (err) => {
+    console.log("");
     err ? console.log(err) : console.log("Generated logo.svg");
+    console.log("");
   });
 }
 
