7d36cb2fb5f9a0c165f47cd664ef56373f52f9db
Renan <rs.miranda93@gmail.com>
2023-05-17 20:17:15 -0400

    final

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 // Include packages needed for this application
 const inquirer = require("inquirer");
 const fs = require("fs");
+const { generateSVG } = require("./lib/generateSVG");
+const { Triangle, Square, Circle } = require("./lib/shapes");
 
 //npm package to vaild for CSS3 compatible color value.
 const isCss3Color = require("is-css3-color");
@@ -12,10 +14,9 @@ init();
 function init() {
   inquirer
     .prompt([
-      
       {
         type: "input",
-        name: "text",
+        name: "logoName",
         message: "Please enter 1-3 characters for your logo: ",
       },
       
@@ -27,9 +28,9 @@ function init() {
       
       {
         type: "list",
-        name: "shape",
+        name: "logoShape",
         message: "Please choose logo shape: ",
-        choices: ["Triangle", "Square", "Circle"],        
+        choices: ["triangle", "square", "circle"],        
       },
       
       {
@@ -43,57 +44,55 @@ function init() {
       let wlogoColor = answers.logoColor.toLowerCase();
 
       // handling of errors that occurred during the entry of information
-      if (answers.text == "") {
+      if (answers.logoName == "") {
         console.log("");
-        console.log("Input text cannot be empty");
+        console.log("Input characters for logo cannot be empty.");
         console.log("");
         init();
-      } else if  (answers.text.length > 3) {
+      } else if  (answers.logoName.length > 3) {
           console.log("");
-          console.log("Logo text cannot be more than 3 characters");
+          console.log("Logo text cannot be more than 3 characters.");
           console.log("");
           init();
       } else if  (!isCss3Color(wtextColor)) {
           console.log("");
-          console.log("Please enter a valid css color keyword or hex code for text color");
+          console.log("Please enter a valid css color keyword or hex code for text color.");
           console.log("");
           init();      
       } else if  (!isCss3Color(wlogoColor)) {
           console.log("");
-          console.log("Please enter a valid css color keyword or hex code for the logo's background color");
+          console.log("Please enter a valid css color keyword or hex code for the logo's background color.");
           console.log("");
           init();
       } else {
-          writeToFile("logo.svg", answers);
+
+          const svgPath = "./logo.svg";
+          let genShape = "";
+    
+          if (answers.logoShape === "circle") {
+            genShape = new Circle(answers);
+          } else if (answers.logoShape === "triangle") {  
+            genShape = new Triangle(answers);
+          } else {
+            genShape = new Square(answers);
+          }
+
+          console.log("");
+        //Generate the svg logo here.
+
+          fs.writeFile(svgPath, generateSVG(genShape), function (err) {
+            if (err) throw err;
+            console.log('Generated logo.svg!');
+            console.log("");
+          });
+
+         // fs.writeFile(svgPath, generateSVG(genShape), (err) =>
+          //  err ? console.error(err) : console.log("Generated logo.svg")
+          //);
+          
       }
-    });
+    })
+    .catch((err) => console.error(err));
 }
 
-// Function to write the SVG file with the user answers
-function writeToFile(fileName, answers) {
-  
-  let generateSVG = "";
- 
-  generateSVG = '<svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg"><g>';
-  generateSVG += `${answers.shape}`;
-
-  if (answers.shape === "Triangle") {
-      generateSVG += `<polygon points="150,5  274,172  26,172" fill="${answers.logoColor}"/>`;
-  } else if (answers.shape === "Square") {
-      generateSVG += `<rect x="60" y="13" width="180" height="180" fill="${answers.logoColor}"/>`;
-  } else {
-      generateSVG += `<circle cx="150" cy="100" r="90" fill="${answers.logoColor}"/>`;
-  }
-
-  generateSVG += `<text x="150" y="114" text-anchor="middle" font-size="45" fill="${answers.textColor}">${answers.text}</text>`;
-  generateSVG += "</g>";
-  generateSVG += "</svg>";
-
-  // Using file system module to generate svg file, takes in file name given in the init function, the svg string, and a ternary operator which handles logging any errors, or a "Generated logo.svg" message to the console  
-  fs.writeFile(fileName, generateSVG, (err) => {
-    console.log("");
-    err ? console.log(err) : console.log("Generated logo.svg");
-    console.log("");
-  });
-}
 
